466666424170667f3bbc944cfe3f190bb5c176d0
Kevin Ngo <quocdatngo2003@gmail.com>
2025-04-08 14:40:35 +1000

    Deleted Tailwind CSS, using material UI instead

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Register from './components/register';
-import './App.css';
 
 function App() {
   return (

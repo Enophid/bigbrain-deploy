d9af2b31b3cd333515d0881202128f47b5262616
Tri Nguyen <88358142+Tris2k4@users.noreply.github.com>
2025-04-22 05:42:52 +1000

    Add Logout component to handle user logout functionality. This new component integrates a LogoutButton and LogoutDialog, utilizing a custom hook for managing logout state and interactions, enhancing the user experience in the application.

---
+++ frontend/src/components/Logout/index.jsx
@@ -0,0 +1,19 @@
+import LogoutButton from './components/LogoutButton';
+import LogoutDialog from './components/LogoutDialog';
+import useLogout from './components/useLogout.jsx';
+
+export default function Logout() {
+  const { open, isLoggingOut, handleOpenDialog, handleCloseDialog, handleLogout } = useLogout();
+
+  return (
+    <>
+      <LogoutButton onLogout={handleOpenDialog} />
+      <LogoutDialog 
+        open={open} 
+        isLoggingOut={isLoggingOut}
+        onClose={handleCloseDialog}
+        onConfirm={handleLogout}
+      />
+    </>
+  );
+} 

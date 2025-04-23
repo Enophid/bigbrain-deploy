5d31da98dd88eef6d8c3c970c758cc61a0c22483
Tri Nguyen <88358142+Tris2k4@users.noreply.github.com>
2025-04-23 19:35:49 +1000

    Update CORS configuration and BACKEND_URL for deployment consistency
    - Modified CORS settings in both vercel.json and server.js to restrict access to a specific origin, enhancing security.
    - Adjusted BACKEND_URL in backend.config.json and frontend/backend.config.json to ensure consistent URL formatting for deployment.
    
    These changes improve the application's security and reliability in connecting to the backend service.

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ import {
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://z5481840-bigbrain-fe-deploy.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {

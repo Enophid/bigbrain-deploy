bd6c1b2452bf453b29c3416d380f2dbd9d991484
Tri Nguyen <88358142+Tris2k4@users.noreply.github.com>
2025-04-24 19:55:53 +1000

    Refactor logout functionality to improve user experience
    - Replaced the direct logout handling with navigation to the login page after successful logout.
    - Added removal of token and admin data from local storage upon logout.
    - Integrated useNavigate from react-router-dom for better routing management.
    
    These changes enhance the logout process, ensuring a smoother transition for users after they log out.

--- frontend/src/components/Logout/components/useLogout.jsx
+++ frontend/src/components/Logout/components/useLogout.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { useAuth } from '../../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 import ApiCall from '../../apiCall';
 
 export default function useLogout() {
-  const { logout } = useAuth();
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const [isLoggingOut, setIsLoggingOut] = useState(false);
 
@@ -18,22 +18,22 @@ export default function useLogout() {
   const handleLogout = async () => {
     setIsLoggingOut(true);
     try {
-      const token = localStorage.getItem('token');
       const data = await ApiCall(
         '/admin/auth/logout',
-        { token },
+        { token: localStorage.getItem('token') },
         'POST'
       );
       if (data.error) {
         throw new Error(data.error || 'Logout failed');
       } else {
-        logout();
+        localStorage.removeItem('token');
+        localStorage.removeItem('admin');
+        navigate('/login');
         setIsLoggingOut(false);
         setOpen(false);
       }
     } catch (error) {
       console.error('Logout failed:', error);
-      logout();
     }
   };
 
